6be3dccfbc0b3cdc07af90d740e06eb96f99fcfd
GUSTAVO FERRARI CRISPIM NERI <gustavo_neri@estudante.sesisenai.org.br>
2025-04-24 09:01:05 -0300

    Funções para cada classes definidas!

--- prova.js
+++ prova.js
@@ -1,32 +1,80 @@
-class Personagem{
-    constructor(vida, forca, mana){
+class Personagem {
+    constructor(vida, forca, mana, golpe, especial) {
         this.vida = vida;
         this.forca = forca;
         this.mana = mana;
+        this.golpe = golpe;
+        this.especial = especial;
     }
 }
 
-class Mago extends Personagem{
-  constructor(ataque, defesa,usarMana){
-    this.ataque = ataque;
-    this.defesa = defesa;
-    this.usarMana = usarMana
-  }
+class Mago extends Personagem {
+    constructor(ataque, defesa, mana) {
+        this.ataque = ataque;
+        this.defesa = defesa;
+        this.mana = mana
+    }
+
+    ataque() {
+        this.ataque += 5
+        console.log('Rajada de magia!')
+    }
+
+    defesa() {
+        this.defesa = 0
+        console.log('Poção de imunidade temporariamente indefinida!')
+    }
+
+    manaUsar() {
+        this.mana += 1('unit')
+        console.log('Erga-se!')
+    }
+
+    manaGasta() {
+        if (this.manaUsar)
+            this.mana -= 50
+        console.log('Pouca energia!')
+    }
 }
 
-class Guerreiro extends Personagem{
-  constructor(ataque, defesa, golpe){
-    this.ataque = ataque;
-    this.defesa = defesa;
-    this.golpe = golpe;
-  }
-}
+class Guerreiro extends Personagem {
+    constructor(ataque, defesa, golpe) {
+        this.ataque = ataque;
+        this.defesa = defesa;
+        this.golpe = golpe;
+    }
+    ataque() {
+        this.ataque += 10
+        console.log('Ataque!')
+    }
 
-class Arqueiro extends Personagem{
-    constructor(ataque, defesa, especial){
+    defesa() {
+        this.vida -= 2
+        console.log('Defesa!')
+    }
+
+    golpe() {
+        this.golpe += 20
+        console.log('Golpe!')
+    }
+
+}
+class Arqueiro extends Personagem {
+    constructor(ataque, defesa, especial) {
         this.ataque = ataque;
         this.defesa = defesa;
         this.especial = especial;
     }
 
+    ataque(){
+        this.ataque += 5
+        console.log('Dano!')
+    }
+
+    defesa(){
+        if(this.ataque = false)
+            this.vida -= 5
+        console.log('Raios de defesa!')
+    }
 }
+//Cada classe tem uma função e reação diferente para cada tipo de ataque ou dano recebido.
