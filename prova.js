92c27f72715758ff94a3084336b0ec3d5ec0f3b3
GUSTAVO FERRARI CRISPIM NERI <gustavo_neri@estudante.sesisenai.org.br>
2025-04-24 08:04:58 -0300

    Criação de cada função das classes

--- prova.js
+++ prova.js
@@ -7,13 +7,26 @@ class Personagem{
 }
 
 class Mago extends Personagem{
-
+  constructor(ataque, defesa,usarMana){
+    this.ataque = ataque;
+    this.defesa = defesa;
+    this.usarMana = usarMana
+  }
 }
 
 class Guerreiro extends Personagem{
-
+  constructor(ataque, defesa, golpe){
+    this.ataque = ataque;
+    this.defesa = defesa;
+    this.golpe = golpe;
+  }
 }
 
 class Arqueiro extends Personagem{
+    constructor(ataque, defesa, especial){
+        this.ataque = ataque;
+        this.defesa = defesa;
+        this.especial = especial;
+    }
 
 }
